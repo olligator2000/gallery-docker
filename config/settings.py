a602801e0006d38fee1e8cadf5d085a9a72f2487
olligator2000 <oleg_luki@mail.ru>
2026-09-04 10:10:25 +0300

    29

--- config/settings.py
+++ config/settings.py
@@ -172,7 +172,7 @@ USE_TZ = True
 # 10. Static files (CSS, JavaScript, Images)
 # ============================================================
 STATIC_URL = '/staticfiles/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'staticfiles']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
